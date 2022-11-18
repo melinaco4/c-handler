e9b333060eb57fdd4e088cfce451f69d2f5f1b8e
melinaco4 <melinaco4@gmail.com>
2022-11-18 21:22:11 +0200

    added server health

--- cmd/main.go
+++ cmd/main.go
@@ -7,11 +7,22 @@ import (
 	"github.com/melinaco4/c-handler/pkg/controllers"
 )
 
+func setupRoutes(app *fiber.App) {
+
+	app.Get("/", controllers.status)
+
+	app.Get("/companies", controllers.GetCompanies)
+	app.Post("/company", controllers.CreateCompany)
+	app.Patch("/company/:id", controllers.UpdateCompany)
+	app.Delete("/company/:id", controllers.DeleteCompany)
+}
+
 func main() {
 	if err := controllers.Connect(); err != nil {
 		log.Fatal(err)
 	}
 	app := fiber.New()
 
-	routes.setupRoutes(app)
+	setupRoutes(app)
+	app.Listen(3000)
 }
